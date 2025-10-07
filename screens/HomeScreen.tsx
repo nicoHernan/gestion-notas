7446b03664c2a96b90b003f1a8f8529f870f372d
nicolas hernan <nicohernanbecco@gmail.com>
2025-10-06 21:12:39 -0300

    feature: add Flat List

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Text, View, Alert, Image, StyleSheet } from 'react-native';
+import {Text, View, Alert, Image, StyleSheet, FlatList } from 'react-native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { RootStackParamList } from './NavigationTypes';
 import Task from '../components/Task';
@@ -13,13 +13,31 @@ type HomeScreenProps = {
 
 const tasks: TaskModel[] = [
     { id: '1', content: 'Comprar martillo', isCompleted: false },
-    { id: '2', content: 'estudiar programación', isCompleted: false }
+    { id: '2', content: 'estudiar programación', isCompleted: false },
+    { id: '3', content: 'estudiar ingles', isCompleted: false },
+    { id: '4', content: 'cocinar', isCompleted: false },
+    { id: '5', content: 'correr', isCompleted: false },
+    { id: '6', content: 'lavar la ropa', isCompleted: false },
+    { id: '7', content: 'pescar', isCompleted: false },
+    { id: '8', content: 'futbol', isCompleted: false },
+    { id: '9', content: 'dormir', isCompleted: false },
+    { id: '10', content: 'ver tv', isCompleted: false },
+    { id: '11', content: 'examen', isCompleted: false },
+    { id: '12', content: 'andar en bici', isCompleted: false }
 ];
 
 const HomeScreen: React.FC<HomeScreenProps> = ({navigation}) => {
 const handleGoToEditTask = (taskModel: TaskModel) => {
         navigation.navigate('Edit', {taskModel} );
     };
+
+    const renderTaskItem = ({ item }: { item: TaskModel }) => (
+        <Task
+            key={item.id}
+            taskModel={item}
+            onPress={() => handleGoToEditTask(item)}
+        />
+    );
 
     return (
         <View style = {styles.container}>
@@ -34,13 +52,12 @@ const handleGoToEditTask = (taskModel: TaskModel) => {
                 buttonTitle="Next"
                 onPress={() => Alert.alert('¡here we go!')}
             />
-             {tasks.map( (taskModel) => (
-                <Task
-                    key={taskModel.id}
-                    taskModel={taskModel}
-                    onPress={() => handleGoToEditTask(taskModel)}
-                />
-                ))}
+             <FlatList
+                data={tasks}
+                keyExtractor={(item) => item.id}
+                renderItem={renderTaskItem}
+                style={styles.taskList}
+            />
         </View>
     );
 };
@@ -57,6 +74,13 @@ const styles = StyleSheet.create({
         height: 200,
         borderRadius: 10,
     },
+    taskList: {
+        width: '100%', 
+        flex: 1,
+        marginTop: 10,
+        paddingHorizontal: 20,
+        paddingBottom: 20
+    }
 });
 
 export default HomeScreen ;
